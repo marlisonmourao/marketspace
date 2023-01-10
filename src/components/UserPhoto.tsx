caa22dddf21e52a3150441aa05ced072f27a8637
marlisonmourao <bentesmourao@gmail.com>
2023-01-10 07:42:58 -0400

    adding properties to the userPhoto component

--- src/components/UserPhoto.tsx
+++ src/components/UserPhoto.tsx
@@ -2,16 +2,18 @@ import { Image, IImageProps } from 'native-base'
 
 type Props = IImageProps & {
   size: number;
+  borderColor?: string;
+  borderWidth?: number;
 }
 
-export function UserPhoto({ size, ...rest }: Props) {
+export function UserPhoto({ size, borderColor = "blue.400", borderWidth = 3, ...rest }: Props) {
   return(
     <Image 
       w={size}
       h={size}
       rounded="full"
-      borderWidth={3}
-      borderColor="blue.400"
+      borderWidth={borderWidth}
+      borderColor={borderColor}
       {...rest}
     />
   )
